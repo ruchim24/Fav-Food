4e69e45741f8178ced4bcf809a1a3ec3a9d5868f
root <ruchimajoka24@gmail.com>
2020-07-22 09:56:52 +0000

    second commit

---
+++ v2/app.js
@@ -0,0 +1,71 @@
+var express = require("express");
+var app     = express();
+var bodyParser = require('body-parser');
+var mongoose   = require("mongoose");
+
+mongoose.connect('mongodb://localhost:27017/Fav_Food', {useNewUrlParser: true});
+app.use(express.static(__dirname + "/public"));
+
+app.use(bodyParser.urlencoded({extended: true}));
+app.set("view engine","ejs");
+
+// var foods = [
+//         {name: "Chole Bhature", image: "https://smedia2.intoday.in/aajtak/images/stories/072015/chole_bhature_pakwan_520_070815033739.jpg"},
+//         {name: "Pav Bhaji", image: "https://encrypted-tbn0.gstatic.com/images?q=tbn%3AANd9GcQfQ4u4FIkY-SJkv6SoX5i7Q4hltUCFQ9jw4g&usqp=CAU"},
+//         {name: "Panner Butter Masala", image: "https://encrypted-tbn0.gstatic.com/images?q=tbn%3AANd9GcShbjNXnSwT17GKha-eL8fGER1j4ZeOrCx5JQ&usqp=CAU"}
+        
+// ];
+
+var foodSchema = new mongoose.Schema({
+	name : String,
+	image: String,
+	description:String
+});
+var Food = mongoose.model("Food",foodSchema);
+
+app.get("/",function(req,res){
+	res.render("home");
+});
+
+app.get("/food",function(req,res){
+	Food.find({},function(err,allFood){
+		if(err){
+			console.log(err);
+		}else{
+			res.render("food",{foods:allFood});
+		}
+	});
+});
+
+ app.post("/food",function(req,res){
+	 var name = req.body.name;
+	 var image= req.body.image;
+	 var desc = req.body.description;
+	 var newFood = {name:name,image:image,description:desc};
+	Food.create(newFood,function(err,newlyCreated){
+		if(err){
+			console.log("err");
+		}else{
+			res.redirect("/food");
+		}
+	});
+ });
+
+app.get("/food/new",function(req,res){
+    res.render("new");	
+});
+
+app.get("/food/:id",function(req,res){
+	Food.findById(req.params.id,function(err,foundFood){
+	if(err){
+		console.log(err);
+	}else{
+		res.render("show",{food:foundFood});
+	}
+	});
+});
+app.listen(3000,function(){
+	
+	console.log("Server listening on port " + this.address().port);
+	
+})
