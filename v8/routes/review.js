aae14c3ed2f13901c66249177ef1e5662f856aed
root <ruchimajoka24@gmail.com>
2020-07-31 19:16:54 +0000

    eighth commit

---
+++ v8/routes/review.js
@@ -0,0 +1,45 @@
+var express = require("express");
+var router  = express.Router({mergeParams: true});
+var Food = require("../models/food");
+var Review = require("../models/review");
+
+router.get("/new",isLoggedIn,function(req,res){
+	Food.findById(req.params.id,function(err,food){
+		if(err){
+			console.log(err);
+		}else{
+			res.render("reviews/new",{food:food});
+		}
+	})
+});
+
+router.post("/",isLoggedIn,function(req,res){
+	Food.findById(req.params.id,function(err,food){
+		if(err){
+			console.log(err);
+			res.redirect("/food");
+		}else{
+		 Review.create(req.body.review,function(err,review){
+			 if(err){
+				 console.log(err);
+			 }else{
+				 review.author.id = req.user._id;
+               review.author.username = req.user.username;
+				
+				 review.save();
+				 food.reviews.push(review);
+				 food.save();
+				 res.redirect("/food/" + food._id);
+			 }
+		 });
+		}
+	});
+});
+
+function isLoggedIn(req, res, next){
+    if(req.isAuthenticated()){
+        return next();
+    }
+    res.redirect("/login");
+}
+module.exports = router;
